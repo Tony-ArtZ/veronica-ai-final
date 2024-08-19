5e700fc018f8cc4c3e770f6959f7eaee7ee706e3
Tony-ArtZ <adarshgku@gmail.com>
2024-08-20 04:43:30 +0530

    fixed user

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import { verifyAccessToken } from "../utils/jwt-helper.js";
-import { getAllPreferences } from "../utils/user-prefs-helper.js";
+import { getAllPreferences, deletePreferenceHelper } from "../utils/user-prefs-helper.js";
 dotenv.config();
 
 const router = express.Router();
@@ -10,7 +10,20 @@ router.get("/prefs", verifyAccessToken, async (req, res, next) => {
   try {
     const userId = req.payload.aud;
     const userPreferences = await getAllPreferences(userId);
-    res.json(userPreferences);
+    const jsonToReturn = {data:Object.fromEntries(userPreferences)};
+    res.json(jsonToReturn);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/prefs/:key", verifyAccessToken, async (req, res, next) => {
+  try {
+    const key = req.params.key;
+    const userId = req.payload.aud;
+    const deletePreferenceResult = await deletePreferenceHelper(userId, key);
+    console.log(deletePreferenceResult)
+    res.json(deletePreferenceResult);
   } catch (err) {
     next(err);
   }
